fa094dfbcc9f135f79570f3b29dbaee6da187608
janu47537 <janu47537@gmail.com>
2024-12-09 15:06:56 +0530

    Add files via upload

---
+++ Access1din2darray.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Access1din2darray{
+    public static void main(String[]args){
+        //initialization of 2D array
+        int a[][]={{1,2},{3,4}};
+        //accessing 1D in 2D array
+        System.out.println(Arrays.toString(a[0]));
+         System.out.println(Arrays.toString(a[1]));
+    }
+    }
